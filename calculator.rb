dd583acbefdf2f4d768779305768ba84dfd7e9e7
Salem Asrat <salemasrat@gmail.com>
2019-09-27 05:09:30 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,4 +5,5 @@ describe "./calculator.rb" do
     first_number = get_variable_from_file('./calculator.rb', "first_number")
 
     expect(first_number).to be_an(Integer).or be_a(Float)
-  
+  end
+end  
